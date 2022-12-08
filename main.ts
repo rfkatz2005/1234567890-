f2e8ab7897d51448a0abc6dfcdbb645c4c332c42
rfkatz2005 <118201375+rfkatz2005@users.noreply.github.com>
2022-12-08 09:16:53 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,5 @@
+function function1 () {
+    return 0
+}
+let name = game.askForString("What's your name?", 7)
+game.splash("Hello " + name + ", I am A1 Sauce the Problem Solver, I am here to help!")
